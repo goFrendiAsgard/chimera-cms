2737cdd756a29bd759b3e4473f33c0aa881d42b1
goFrendiAsgard <goFrendiAsgard@gmail.com>
2018-02-27 09:44:25 +0700

    add thirdPartyLogin mechanism

--- chains/core.login-api.js
+++ chains/core.login-api.js
@@ -1,4 +1,48 @@
 const jsonwebtoken = require('jsonwebtoken')
+const invalidLoginResponse = {
+  data: {
+    token: '',
+    status: 400,
+    userMessage: 'Invalid username or password',
+    developerMessage: 'Invalid username or password'
+  }
+}
+
+function createLoggedInResponse (ins, vars, user) {
+  let state = ins[0]
+  let $ = vars.$
+  let {config, request} = state
+  let jwtSecret = config.jwtSecret
+  let expiresIn = config.jwtExpired
+  let auth = $.helper.getLoggedInAuth(user)
+  let jwtToken = jsonwebtoken.sign(auth, jwtSecret, { expiresIn })
+  let cookies = {}
+  cookies[config.jwtTokenName] = jwtToken
+  response = {
+    cookies,
+    auth,
+    data: {
+      token: jwtToken,
+      status: 200,
+      userMessage: 'Login success',
+      developerMessage: 'Login success'
+    }
+  }
+  return response
+}
+
+function thirdPartyLogin (ins, vars, callback) {
+  let state = ins[0]
+  let $ = vars.$
+  let {config, request} = state
+  if (config.thirdPartyLoginChain) {
+    return $.runChain(config.thirPartyLoginChain, ...ins, (error, user) => {
+      let response = user ? createLoggedInResponse(ins, vars, user) : invalidLoginResponse
+      callback(error, response)
+    })
+  }
+  return callback(null, invalidLoginResponse)
+}
 
 module.exports = (ins, vars, callback) => {
   let state = ins[0]
@@ -7,37 +51,17 @@ module.exports = (ins, vars, callback) => {
   let identity = request.query.user || request.body.user
   let password = request.query.password || request.body.password
   $.helper.mongoExecute('web_users', 'find', {$and: [{_deleted: {$ne: 1}}, {$or: [{username: identity}, {email: identity}]}]}, (error, users) => {
-    let response = {
-      data: {
-        token: '',
-        status: 400,
-        userMessage: 'Invalid username or password',
-        developerMessage: 'Invalid username or password'
-      }
-    }
+    let response = invalidLoginResponse
     if (users.length > 0) {
       let user = users[0]
       let salt = user.salt
       let hashedObject = $.helper.hashPassword(password, salt)
       if (hashedObject.hashedPassword === user.hashedPassword) {
-        let jwtSecret = config.jwtSecret
-        let expiresIn = config.jwtExpired
-        let auth = $.helper.getLoggedInAuth(user)
-        let jwtToken = jsonwebtoken.sign(auth, jwtSecret, {expiresIn})
-        let cookies = {}
-        cookies[config.jwtTokenName] = jwtToken
-        response = {
-          cookies,
-          auth,
-          data: {
-            token: jwtToken,
-            status: 200,
-            userMessage: 'Login success',
-            developerMessage: 'Login success'
-          }
-        }
+        let response = createLoggedInResponse(ins, vars, user)
+        return callback(error, response)
       }
+      return thirdPartyLogin(ins, vars, callback)
     }
-    callback(error, response)
+    return thirdPartyLogin(ins, vars, callback)
   })
 }
