2d8dfa9e9c2196175866acc79787f83144a17a0f
goFrendiAsgard <goFrendiAsgard@gmail.com>
2018-03-03 10:50:42 +0700

    change chimera-framework version

---
+++ chains/total-kanban/card.init.js
@@ -0,0 +1,35 @@
+const tkHelper = require('./tkHelper.js')
+
+module.exports = (ins, vars, callback) => {
+  let cckState = ins[0]
+  let state = ins[1]
+  let $ = vars.$
+  let request = state.request
+  let dbConfig = {
+    collectionName: 'tk_label_types', 
+    dbOption: { excludeDeleted: 0, showHistory: 0 }
+  }
+  // get only universal labels
+  let filter = {board: { $exists: false}}
+  // but in case of insert/update, make sure the exclusive labels for the board are also included
+  if (request.body.board) {
+    filter = {$or: [filter, {board: request.body.board}]}
+  }
+  $.helper.mongoExecute(dbConfig, 'find', filter, (error, labelTypes) => {
+    if (error) {
+      return callback(error, cckState)
+    }
+    cckState = tkHelper.initLabeledCardCckState(ins, vars, labelTypes)
+    // add request.body to data
+    for (let fieldName of cckState.fieldNames) {
+      if (fieldName in request.body && !(fieldName in cckState.data) && !(fieldName in cckState.unset)) {
+        if (request.body[fieldName] !== '') {
+          cckState.data[fieldName] = request.body[fieldName]
+        } else {
+          cckState.unset[fieldName] = ''
+        }
+      }
+    }
+    return callback(null, cckState)
+  })
+}
