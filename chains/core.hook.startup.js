4b056724e25b3f3ac77c3f531c4f4dd5c6ad12ff
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-03-30 16:35:59 +0700

    add `db connection failed message`

--- chains/core.hook.startup.js
+++ chains/core.hook.startup.js
@@ -1,5 +1,25 @@
+'use strict'
+
+const monk = require('monk')
+const path = require('path')
+
 module.exports = (ins, vars, callback) => {
   let state = ins[0]
   let $ = vars.$
-  $.helper.injectState(state, callback)
+  let mongoUrl = state.config.mongoUrl
+  let dbTest = monk(mongoUrl, {}, (error) => {
+    if (error) {
+      // db connection failed, use fallback scenario
+      state.config.routes = [{
+        route: '/',
+        method: 'all',
+        groups: ['loggedIn', 'loggedOut'],
+        chain: path.join(state.config.chainPath, 'fallback.chiml')
+      }]
+      state.fallback = true
+      return callback(null, state)
+    }
+    dbTest.close()
+    $.helper.injectState(state, callback)
+  })
 }
