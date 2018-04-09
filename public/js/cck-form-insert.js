906b30ceb46d9a97d363050e1ce127e24a535a04
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-04-09 22:11:26 +0700

    split up javascript for update and insert

---
+++ public/js/cck-form-insert.js
@@ -0,0 +1,41 @@
+/* eslint-env jquery */
+
+if (typeof $ === 'undefined') {
+  var cwAjaxifyForm = () => {}
+}
+
+function cwAjaxifyInsertForm (schemaName) {
+  $(document).ready(function () {
+    // ajaxify
+    cwAjaxifyForm('cw-form', {
+      method: 'POST',
+      action: '/api/v1/' + schemaName,
+      dataType: 'json',
+      success: function (data, textStatus, jqXhr) {
+        $('#cw-btn-re-edit').hide()
+        if (data.status < 400) {
+          $('#cw-modal-alert').removeClass('alert-danger')
+          $('#cw-modal-alert').addClass('alert-success')
+          $('#cw-modal-alert').html('<strong>Success</strong> ' + data.userMessage)
+          if ('_id' in data.result) {
+            $('#cw-btn-re-edit').attr('href', '/data/<%= cckState.schemaName %>/update/' + data.result._id + '?excludeDeleted=<%= cckState.excludeDeleted%>&limit=<%= cckState.limit %>')
+            $('#cw-btn-re-edit').show()
+          }
+        } else {
+          $('#cw-modal-alert').removeClass('alert-success')
+          $('#cw-modal-alert').addClass('alert-danger')
+          $('#cw-modal-alert').html('<strong>Failed</strong> ' + data.userMessage)
+          console.log(data)
+        }
+        $('#cw-modal').modal('show')
+      },
+      error: function (jqXhr, textStatus, errorThrown) {
+        $('#cw-modal-alert').removeClass('alert-success')
+        $('#cw-modal-alert').addClass('alert-danger')
+        $('#cw-modal-alert').html('<strong>Failed</strong> ' + textStatus)
+        console.error(errorThrown)
+        $('#cw-modal').modal('show')
+      }
+    })
+  })
+}
